5ed75cac0101ece0cde1dd1b08ca51661502af99
elevUser <117316029+elevUser@users.noreply.github.com>
2023-06-22 14:56:54 +0200

    Update My_stm32f4_uart_lib.h

--- Bibliotek/My_stm32f4_uart_lib.h
+++ Bibliotek/My_stm32f4_uart_lib.h
@@ -1,8 +1,8 @@
 
-#ifndef		__MY_STM32F4_GPIO_LIB_H__
-#define		__MY_STM32F4_GPIO_LIB_H__
+#ifndef		__MY_STM32F4_UART_LIB_H__
+#define		__MY_STM32F4_UART_LIB_H__
 
-#include  "my_stm32f4_gpio_driver.h"
+#include  "My_stm32f4_uart_driver.h"
 
 #define GPIO_PIN_0				((uint16_t)0x0001) 
 #define GPIO_PIN_1				((uint16_t)0x0002) 
